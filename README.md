efd0756869a70e669ef0c30315cbcec67a033c0a
s.rostovcev <s.rostovcev@owen.ru>
2023-09-05 16:08:18 +0300

    Init

---
+++ README.md
@@ -0,0 +1,6 @@
+# node-red-contrib-mf-mavlink
+
+a node-red node that wraps the node.js MAVlink library to control drones.
+
+see https://github.com/omcaree/node-mavlink
+the mavlink protocol messages http://mavlink.org/messages/common
---
+++ mf-mavlink.js
@@ -0,0 +1,104 @@
+const mavlink = require('mavlink');
+
+module.exports = function(RED) {
+  function MavlinkNode(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    this.version = config.version;
+    //TODO set the version of the protocol in the node...
+    var myMAV = new mavlink(1, 1, this.version, ["common","pixhawk"]);
+    var messagesListened = []; // a list of message that we want to listen to
+
+    this.on('input', function(msg) {
+		      	//this.log('Parse mavlink');
+
+      //if we get a buffer, we consider that the node is receiving UDP feed
+      if (Buffer.isBuffer(msg.payload)){
+      	//this.log('Parse mavlink raw');
+	//listen for messages
+	/*myMAV.on('message', function(message) {
+		var msg = {payload:{}};
+		msg.payload = message.payload;
+		console.log(message);
+		node.send(msg);
+	});*/
+	myMAV.on('ATTITUDE', function(message, fields) {
+		//console.log(fields);
+		var msg = {payload:{}, name: 'ATTITUDE'};
+		msg.payload = fields;
+		node.send(msg);
+	});
+	myMAV.on('GPS_RAW_INT', function(message, fields) {
+		//console.log(fields);
+		var msg = {payload:{}, name: 'GPS_RAW_INT'};
+		msg.payload = fields;
+		node.send(msg);
+	});	
+	myMAV.on('VFR_HUD', function(message, fields) {
+		console.log(fields);
+		var msg = {payload:{}, name: 'VFR_HUD'};
+		msg.payload = fields;
+		node.send(msg);
+	});	
+	/*myMAV.on('HEARTBEAT', function(message, fields) {
+		console.log(fields);
+		var msg = {payload:{}};
+		msg.payload = fields;
+		node.send(msg);
+	});
+	myMAV.on('COMMAND_LONG', function(message, fields) {
+		console.log(fields);
+		var msg = {payload:{}};
+		msg.payload = fields;
+		node.send(msg);
+	});*/
+	//listen for sequenceError
+	myMAV.on('sequenceError', function(message) {
+		//this.log('sequenceError');
+		msg.payload = 'sequenceError';
+		node.send(msg);
+	});
+	//listen for checksumFail
+	myMAV.on('checksumFail', function(message) {
+		//this.log('checksumFail');
+		msg.payload = 'checksumFail';
+		node.send(msg);
+	});
+        myMAV.parse(msg.payload);
+        myMAV.removeAllListeners(['ATTITUDE']);
+        myMAV.removeAllListeners(['GPS_RAW_INT']);		
+        myMAV.removeAllListeners(['VFR_HUD']);		
+        myMAV.removeAllListeners(['message']);
+        myMAV.removeAllListeners(['sequenceError']);
+        myMAV.removeAllListeners(['checksumFail']);
+      } else if (Array.isArray(msg.payload)) { //is not a buffer if its an array we store messages that MAVlink needs to listen to
+
+        //detaching previous events
+        for (var i=0 ; i < messagesListened.length ; i++){
+            myMAV.on(messagesListened[i], function(){});
+        }
+        //updating the new messages to listen to
+        messagesListened = msg.payload;
+        for (var i=0 ; i < messagesListened.length ; i++){
+            myMAV.on(messagesListened[i], function(message, fields) {
+              msg.topic = myMAV.getMessageName(message.id);
+              msg.payload = fields;
+              node.send(msg);
+            });
+        }
+
+      } else { //finally it should be a JSON input that has to be turned into MAVlink binary format...
+      	this.log('Parse mavlink other');
+      myMAV.createMessage(msg.payload.name, msg.payload.parameters,
+        function(mavMessage) {
+          msg.payload = mavMessage.buffer;
+          node.send(msg);
+        }
+      );
+
+    }
+
+    });
+  }
+  RED.nodes.registerType("MAVlink", MavlinkNode);
+}
